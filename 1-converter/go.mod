7aec83d6f412a398dc31b89580ffd793a513f861
Foga <efoger@gmail.com>
2025-05-14 01:15:06 +0300

    1-converter dz

---
+++ 1-converter/go.mod
@@ -0,0 +1,3 @@
+module dz
+
+go 1.24.3
---
+++ 1-converter/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	const EUR float64 = 0.89
+	const RUB float64 = 79.85
+
+	var eurAmount float64 = 10
+
+	usdAmount := eurAmount * EUR
+	rubAmount := usdAmount * RUB
+
+	fmt.Printf("RUB: %.2f\n", rubAmount)
+}
