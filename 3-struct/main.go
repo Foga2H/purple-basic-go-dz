b3115a8cf6aa041d53bccd949523cfd07b63068c
Foga <efoger@gmail.com>
2025-05-19 20:50:58 +0300

    interfaces

--- 3-struct/main.go
+++ 3-struct/main.go
@@ -1,12 +1,14 @@
 package main
 
-import "time"
+import "dz/3-struct/bins"
 
-type Bin struct {
-	id        string
-	private   bool
-	createdAt time.Time
-	name      string
+type FileService interface {
+	ReadFile(path string) ([]byte, error)
+}
+
+type StorageService interface {
+	SaveBinList(path string, bins bins.Bins) error
+	ReadBinList(path string) (bins.Bins, error)
 }
 
 func main() {
